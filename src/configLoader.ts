43acceb0d2e944808dba32fe867b77327bb4fba0
Bohao Wang <bj050323@gmail.com>
2022-07-10 14:56:29 +0800

    feat: workspace settings

--- src/configLoader.ts
+++ src/configLoader.ts
@@ -7,38 +7,59 @@ import { CoreData } from './coreData';
 export class ConfigLoader {
 	// get config from location
 	static getConfigLocation() {
-		return vscode.workspace.getConfiguration('n18n').get('location');
+		if (vscode.workspace.workspaceFolders) {
+			return vscode.workspace.getConfiguration(
+				'n18n',
+				vscode.workspace.workspaceFolders[0]
+			).get('location');
+		}
+		return '';
 	}
 
-	static setup() {
+	static async setup() {
 		let openDialogOptions: vscode.OpenDialogOptions = {
 			canSelectFiles: true,
 			canSelectFolders: false,
 			canSelectMany: false,
 			openLabel: 'Select',
+			/* eslint-disable */
 			filters: {
 				"JSON": ['json'],
 				"JavaScript": ['js'],
 			}
+			/* eslint-enable */
 		};
 
-		return vscode.window.showOpenDialog(openDialogOptions).then(fileUri => {
-			if (fileUri && fileUri[0]) {
-				let n18nConfiguration = vscode.workspace.getConfiguration('n18n');
-			 	n18nConfiguration.update('location', path.normalize(fileUri[0].fsPath), true).then(() => {
-					// prompt to reload window so storage location change can take effect
-					vscode.window.showWarningMessage(
-						`You must reload the window for the storage location change to take effect.`, 
-						'Reload'
-					).then(selectedAction => {
-						// if the user selected to reload the window then reload
-						if (selectedAction === 'Reload') {
-							vscode.commands.executeCommand('workbench.action.reloadWindow');
-						}
-					});
+		const fileUri = await vscode.window.showOpenDialog(openDialogOptions);
+		if (fileUri && fileUri[0]) {
+			let workspaceFolders = vscode.workspace.workspaceFolders;
+			if (!workspaceFolders) {
+				const folder = await vscode.window.showWorkspaceFolderPick({
+					placeHolder: 'Pick Workspace Folder to which this setting should be applied',
 				});
+				if (!folder) { return; }
+				workspaceFolders = [folder];
 			}
-		});
+
+			let n18nConfiguration = vscode.workspace.getConfiguration('n18n', workspaceFolders[0]);
+			await n18nConfiguration.update(
+				'location',
+				path.normalize(fileUri[0].fsPath),
+				vscode.ConfigurationTarget.Workspace,
+				true
+			);
+
+			// prompt to reload window so storage location change can take effect
+			const selectedAction = await vscode.window.showWarningMessage(
+				`You must reload the window for the storage location change to take effect.`,
+				'Reload'
+			);
+
+			// if the user selected to reload the window then reload
+			if (selectedAction === 'Reload') {
+				vscode.commands.executeCommand('workbench.action.reloadWindow');
+			};
+		}
 	}
 
 	static reload(data: CoreData) {
@@ -47,21 +68,21 @@ export class ConfigLoader {
 		if (!configLocation) { return; }
 
 		// read files in location
-    return new Promise((resolve) => {
-      fs.readFile(String(configLocation), (err, buf) => {
-        if (err) {
-          log.appendLine(err.toString());
-          // don't show warning
-        } else {
-          try {
-            const content = JSON.parse(buf.toString());
-            data.setTexts(content);
-          } catch (err) {
-            vscode.window.showErrorMessage('Bad i18n JSON file, try to run `n18n: setup` again');
-          }
-        }
-        resolve(1);
-      });
-    });
+		return new Promise((resolve) => {
+			fs.readFile(String(configLocation), (err, buf) => {
+				if (err) {
+					log.appendLine(err.toString());
+					// don't show warning
+				} else {
+					try {
+						const content = JSON.parse(buf.toString());
+						data.setTexts(content);
+					} catch (err) {
+						vscode.window.showErrorMessage('Bad i18n JSON file, try to run `n18n: setup` again');
+					}
+				}
+				resolve(1);
+			});
+		});
 	}
 }
