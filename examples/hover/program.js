22c2bcaf918c0c3cfa6d46e2d817a0e4af843855
Bohao Wang <bj050323@gmail.com>
2022-07-11 14:00:57 +0800

    feat: support complex js file

--- examples/hover/program.js
+++ examples/hover/program.js
@@ -2,6 +2,8 @@ let a = 'HELLO';
 
 if (a === i18n('s20001') || a === i18n('s20002')) {
   console.log('s20003');
+  console.log('s20004');
+  console.log('s20005');
 }
 
 const s20001 = 1;
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,23 @@
+import * as acorn from 'acorn';
 import { log } from './log';
+
+type AcornProperty = {
+  type: string;
+  key: {
+    type: string;
+    value: string;
+    name: string;
+  },
+  value: {
+    type: string;
+    value: string;
+  },
+};
+
+function parseKeyValue(key: { type: string, name: string, value: string }): string {
+  if (key.type === 'Identifier') { return key.name; }
+  return key.value;
+}
 
 /**
  * a naive way to extract the default exported js object to json string
@@ -7,16 +26,21 @@ import { log } from './log';
  *   to
  *   { "s00001": "hello" } 
  * @param {string} code JavaScript code 
- * @returns {string} json string
+ * @returns {string}
  */
 export function extractJson(code: string): string {
-  let text = /export\s*default\s*(\{(.|\r|\n)*\})/;
-  let match = code.match(text);
-  if (match) {
-    let text = match[1];
-    text = text.replace(/'(.*)'/g, `"$1"`);
-    log.appendLine(text);
-    return text;
+  const program = acorn.parse(code, { ecmaVersion: 2020, sourceType: 'module' });
+  // @ts-ignore
+  const body: acorn.Node[] = program.body;
+  const exportDefaultDecl = body.find(x => x.type === 'ExportDefaultDeclaration');
+  if (exportDefaultDecl) {
+    // @ts-ignore
+    const properties: AcornProperty[] = exportDefaultDecl.declaration.properties;
+    const result: Record<string, string> = {};
+    properties.forEach(item => {
+      result[parseKeyValue(item.key)] = item.value.value;
+    });
+    return JSON.stringify(result);
   }
   return '{}';
 }
